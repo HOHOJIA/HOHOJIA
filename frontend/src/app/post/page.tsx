4920629a4622265c3e42c46a5c27f627a2f8f080
chengyenn <a0987206119@gmail.com>
2024-04-24 11:51:38 +0800

    update post page - Introduction section

--- frontend/src/app/post/page.tsx
+++ frontend/src/app/post/page.tsx
@@ -1,3 +1,25 @@
-export default function LoginPage() {
-    return <div className="">LoginPage</div>
+"use client";
+import Introduction from "./Introduction";
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+
+export default function Post() {
+  return (
+    <div className="bg-slate-50 w-full h-full flex justify-center">
+      <Card shadow="md" className="py-4 my-12 w-8/12">
+        <form action="">
+          <CardHeader className="py-4 px-12 flex-col items-start">
+            <p className="text-xl font-bold border-b-2 pb-1.5 border-black mb-1.5">
+              來分享你的美味秘訣吧！
+            </p>
+            <Introduction />
+          </CardHeader>
+          <CardBody className="py-4 px-12 flex-col items-start">
+            <p className="text-xl font-bold border-b-2 mb-4 border-black">
+              料理細節
+            </p>
+          </CardBody>
+        </form>
+      </Card>
+    </div>
+  );
 }
