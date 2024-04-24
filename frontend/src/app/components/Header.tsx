ad98a759a7d431ef80be8270a0fb6374e979e834
Yi-Jhen Jie <emily6331@gmail.com>
2024-04-24 16:25:45 +0800

    Feat: Change Header styles and add login/logout button

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -1,70 +1,88 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
-import { Button, Input } from "@nextui-org/react";
+import {
+    Button,
+    Input,
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+} from "@nextui-org/react";
 import { FaSearch } from "react-icons/fa";
 import { FaQuestion } from "react-icons/fa6";
 import { IoPersonSharp } from "react-icons/io5";
 import { TbBellRinging2Filled } from "react-icons/tb";
 
 export default function Header() {
-    const pathname = usePathname();
-
-    function isActive(path: string) {
-        return pathname === path;
-    }
+    const [isLogined, setIsLogined] = useState(false);
 
     return (
-        <div className="z-50 flex flex-row items-center justify-between w-full py-2 bg-white drop-shadow-lg px-7">
-            <Image
-                alt="HOHOJIA"
-                src="/images/logo_HOHOJIA.webp"
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{ width: "10%", height: "auto" }}
-            />
+        <Navbar
+            maxWidth="full"
+            classNames={{
+                item: [
+                    "flex",
+                    "relative",
+                    "h-full",
+                    "items-center",
+                    "text-gray-500",
+                    "data-[active=true]:font-normal",
+                    "data-[active=true]:text-black",
+                    "data-[active=true]:after:absolute",
+                    "data-[active=true]:after:bottom-4",
+                    "data-[active=true]:after:left-0",
+                    "data-[active=true]:after:right-0",
+                    "data-[active=true]:after:h-[3px]",
+                    "data-[active=true]:after:rounded-[2px]",
+                    "data-[active=true]:after:bg-yellow-300",
+                ],
+            }}
+        >
+            <NavbarBrand>
+                <Image
+                    alt="HOHOJIA"
+                    src="/images/logo_HOHOJIA.webp"
+                    width={150}
+                    height={150}
+                />
+            </NavbarBrand>
 
-            <div className="absolute flex gap-10 mx-auto text-sm transform -translate-x-1/2 left-1/2">
-                <Link
-                    href="/"
-                    className={`hover:text-black transition-colors duration-300 ${
-                        isActive("/")
-                            ? "border-b-3 border-yellow-500 text-black"
-                            : "text-gray-500"
-                    }`}
-                >
-                    HOJIA食譜
-                </Link>
-                <Link
-                    href="/"
-                    className={`hover:text-black transition-colors duration-300 ${
-                        isActive("/secondhand")
-                            ? "border-b-3 border-yellow-500 text-black"
-                            : "text-gray-400"
-                    }`}
-                >
-                    二手廚具底JIA
-                </Link>
-            </div>
+            <NavbarContent className="flex gap-10" justify="center">
+                <NavbarItem isActive>
+                    <Link
+                        href="/"
+                        className="transition-colors duration-300 hover:text-black"
+                    >
+                        HOJIA食譜
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link
+                        href="/"
+                        className="transition-colors duration-300 hover:text-black"
+                    >
+                        二手廚具底JIA
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
 
-            <div className="flex items-center gap-5">
+            <NavbarContent className="flex items-center gap-5" justify="end">
                 <Input
                     size="md"
                     key="outside"
                     type=""
                     label=""
                     placeholder="搜尋食譜"
-                    className="mr-2 bg-white"
+                    className="mr-2 w-60"
                     classNames={{
                         label: "",
                         input: "",
                         innerWrapper: "",
                         inputWrapper:
-                            "pr-0 pl-4 border-yellow-300 rounded-md border-1 ",
+                            "bg-white pr-0 pl-4 border-yellow-300 rounded-md border-1",
                     }}
                     endContent={
                         <Button
@@ -76,28 +94,42 @@ export default function Header() {
                         />
                     }
                 />
-                <Button
-                    color="primary"
-                    isIconOnly
-                    size="sm"
-                    radius="full"
-                    startContent={<FaQuestion size={20} />}
-                />
-                <Button
-                    color="primary"
-                    isIconOnly
-                    size="sm"
-                    radius="full"
-                    startContent={<TbBellRinging2Filled size={20} />}
-                />
-                <Button
-                    color="primary"
-                    isIconOnly
-                    size="sm"
-                    radius="full"
-                    startContent={<IoPersonSharp size={20} />}
-                />
-            </div>
-        </div>
+                {!isLogined ? (
+                    <Button
+                        color="primary"
+                        size="md"
+                        radius="sm"
+                        className="px-10"
+                        onClick={() => setIsLogined(true)}
+                    >
+                        登入/註冊
+                    </Button>
+                ) : (
+                    <>
+                        <Button
+                            color="primary"
+                            isIconOnly
+                            size="sm"
+                            radius="full"
+                            startContent={<FaQuestion size={20} />}
+                        />
+                        <Button
+                            color="primary"
+                            isIconOnly
+                            size="sm"
+                            radius="full"
+                            startContent={<TbBellRinging2Filled size={20} />}
+                        />
+                        <Button
+                            color="primary"
+                            isIconOnly
+                            size="sm"
+                            radius="full"
+                            startContent={<IoPersonSharp size={20} />}
+                        />
+                    </>
+                )}
+            </NavbarContent>
+        </Navbar>
     );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from 'next/image'
-import Home from '@/app/components/home/Home'
+import Image from "next/image";
+import Home from "@/app/components/home/Home";
+import Header from "./components/Header";
 
 export default function Index() {
     return (
         <main className="">
+            <Header />
             <Home />
         </main>
-    )
+    );
 }
