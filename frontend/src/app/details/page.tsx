b135c4a3ad74584545a05da6a0528b9aba57ff48
Yi-Jhen Jie <emily6331@gmail.com>
2024-04-25 20:53:49 +0800

    Feat: Add details page comments layout

--- frontend/src/app/details/page.tsx
+++ frontend/src/app/details/page.tsx
@@ -4,11 +4,12 @@ import Ingredients from "../components/details/Ingredients";
 import AuthorInfo from "../components/details/AuthorInfo";
 import Steps from "../components/details/Steps";
 import Tips from "../components/details/Tips";
+import Comments from "../components/details/Comments";
 import Footer from "../components/Footer";
 
 export default function Details() {
     return (
-        <div>
+        <main className="">
             <Header />
 
             <div className="flex flex-col items-start w-full gap-12 px-32 pt-16 pb-40">
@@ -19,9 +20,10 @@ export default function Details() {
                 </div>
                 <Steps />
                 <Tips />
+                <Comments />
             </div>
 
             <Footer />
-        </div>
+        </main>
     );
 }
