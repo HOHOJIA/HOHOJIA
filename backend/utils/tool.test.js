a4dd859bc8a8ec08adc3f2d6eb6b05bc52b84448
pj <Pinjiang1220@gmail.com>
2024-04-27 01:42:25 +0800

    Format tool.test.js

--- backend/utils/tool.test.js
+++ backend/utils/tool.test.js
@@ -1,11 +1,11 @@
 const tool = require('./tool');
 
 describe('Check email', () => {
-test('When given a valid email, check email should pass', async () => {
-    const validEmail = 'hohojia@gmail.com'
+    test('When given a valid email, check email should pass', async () => {
+        const validEmail = 'hohojia@gmail.com'
 
-    const result = await tool.checkEmail(validEmail);
+        const result = await tool.checkEmail(validEmail);
 
-    expect(result).toBe(true);
-});
+        expect(result).toBe(true);
+    });
 });
