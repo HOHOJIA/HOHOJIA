5532782fbd2d9109af0a1d6dc1f16d43ac3eced3
pj <Pinjiang1220@gmail.com>
2024-04-26 23:41:49 +0800

    Add an example unit test for tool.js

---
+++ backend/utils/tool.test.js
@@ -0,0 +1,11 @@
+const tool = require('./tool');
+
+describe('Check email', () => {
+test('When given a valid email, check email should pass', async () => {
+    const validEmail = 'hohojia@gmail.com'
+
+    const result = await tool.checkEmail(validEmail);
+
+    expect(result).toBe(true);
+});
+});
