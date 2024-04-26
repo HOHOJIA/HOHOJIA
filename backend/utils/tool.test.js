b4746ab50220370044fcd3d9f28b0e7a76ecbccb
pj <Pinjiang1220@gmail.com>
2024-04-27 00:03:09 +0800

    Expect this should fail on CI

--- backend/utils/tool.test.js
+++ backend/utils/tool.test.js
@@ -2,7 +2,7 @@ const tool = require('./tool');
 
 describe('Check email', () => {
 test('When given a valid email, check email should pass', async () => {
-    const validEmail = 'hohojia@gmail.com'
+    const validEmail = 'hohojiaXgmail.com'
 
     const result = await tool.checkEmail(validEmail);
 
