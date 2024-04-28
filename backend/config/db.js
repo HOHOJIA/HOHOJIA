d2c1d35b415bb01c5e1dc7ad8db181d4fff909a8
pj <Pinjiang1220@gmail.com>
2024-04-29 00:11:51 +0800

    Add test dotenv

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,13 @@
 const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
-dotenv.config();
+
+dotenv.config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+});
 
 const connectionPromise = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
