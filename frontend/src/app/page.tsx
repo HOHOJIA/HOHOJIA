288bec7427f252ce0248ba35b217d014a5182f65
tsaichiehhuang <a0970605512@gmail.com>
2024-05-02 21:21:15 +0800

    home page add footer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,14 @@
-import Image from "next/image";
-import Home from "@/app/components/home/Home";
-import Header from "./components/Header";
+import Image from 'next/image'
+import Home from '@/app/components/home/Home'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function Index() {
     return (
         <main className="">
             <Header />
             <Home />
+            <Footer />
         </main>
-    );
+    )
 }
