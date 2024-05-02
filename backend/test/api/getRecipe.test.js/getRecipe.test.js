c78ab63428f1e622910875eff5a40768224be822
pj <Pinjiang1220@gmail.com>
2024-05-03 00:24:03 +0800

    Add basic test for get recipe API and modify API path

---
+++ backend/test/api/getRecipe.test.js/getRecipe.test.js
@@ -0,0 +1,33 @@
+const request = require("supertest");
+const app = require("../../../app");
+const executeSql = require('../../testUtils/testUtils').executeSql;
+const connectionPromise = require('../../../config/db').connectionPromise;
+const path = require('path');
+
+describe("GET /api/1.0/recipe", () => {
+
+    beforeAll(async () => {
+        await executeSql(path.join(__dirname, '../testData/clear_test_db.sql'));
+        await executeSql(path.join(__dirname, './testData/init_test_db.sql'));
+    })
+
+    afterAll(async () => {
+        await connectionPromise.end();
+    })
+
+    it("should get recipe", async () => {
+        const existRecipeId = 1;
+
+        const res = await request(app).get(`/api/1.0/recipe/${existRecipeId}`).send({});
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body.data.recipeId).toEqual(existRecipeId);
+    });
+
+    it("should not found recipe", async () => {
+        const notExistId = 2;
+        const res = await request(app).get(`/api/1.0/recipes/${notExistId}`).send({});
+        expect(res.statusCode).toBe(404);
+    });
+});
+
--- backend/test/api/testData/clear_test_db.sql
+++ backend/test/api/testData/clear_test_db.sql
@@ -2,6 +2,7 @@ USE `hohoja`;
 DELETE FROM `steps`;
 DELETE FROM `ingredients`;
 DELETE FROM `recipetags`;
+DELETE FROM `recipecomments`;
 DELETE FROM `recipes`;
 DELETE FROM `tags`;
 DELETE FROM `users`;
