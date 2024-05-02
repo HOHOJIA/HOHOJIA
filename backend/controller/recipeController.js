90ed3f457fc433c9a50bdee0b659533b3217d417
pj <Pinjiang1220@gmail.com>
2024-05-03 00:23:12 +0800

    Implement get recipe API

--- backend/controller/recipeController.js
+++ backend/controller/recipeController.js
@@ -1,5 +1,6 @@
-const { body, validationResult } = require('express-validator');
+const { body, param, validationResult } = require('express-validator');
 const postReceipeHandler = require('../Application/Features/User/Commands/PostRecipe/postRecipeHandler');
+const getRecipeHandler = require('../Application/Features/User/Commands/GetRecipe/getRecipeHandler');
 const errorMsg = require('../utils/errorMsg');
 
 
@@ -21,6 +22,26 @@ module.exports = {
             errorMsg.serverError(res);
         }
     },
+    getRecipe: async (req, res) => {
+        try {
+            const validateResults = validationResult(req);
+            if (!validateResults.isEmpty()) {
+                errorMsg.badRequest(res, validateResults);
+                return;
+            }
+
+            const response = await getRecipeHandler.handle(req, res);
+            if (response) {
+                res.status(200).json(response);
+            }
+        } catch (error) {
+            console.log(error);
+            errorMsg.serverError(res);
+        }
+    },
+    getRecipeValidator: [
+        param('recipeId').notEmpty().isInt()
+    ],
     postRecipeValidator: [
         body('title').notEmpty().isString(),
         body('quantity').notEmpty(),
