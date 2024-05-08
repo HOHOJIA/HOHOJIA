b2709e9b5fe54bf70f272cb6ca4f1d33c95078a1
Lu-weiting <runeeld23@gmail.com>
2024-05-08 22:13:40 +0800

    Fix: remove s3 presigned test and merge develop

--- backend/test/utils/s3Service.test.js
+++ backend/test/utils/s3Service.test.js
@@ -5,20 +5,18 @@ const path = require('path');
 describe('Check email', () => {
     test('test upload to s3', async () => {
         const testDir = path.dirname(__filename);
-
-        const imagePath = path.join(testDir, 'braces.jpg');
+        const imagePath = path.join(testDir, 'i.jpeg');
         const imageBuffer = fs.readFileSync(imagePath);
         const file = {
-            originalname: 'braces.jpg',
+            originalname: 'i.jpeg',
             buffer: imageBuffer,
             mimetype: 'image/jpeg',
         };
 
         const result = await s3.uploadToS3(file);
-        // expect(result).toMatch(/^https:\/\/[a-zA-Z0-9\-.]+\.s3\.[a-z]+\.amazonaws\.com\/[0-9]+-fang.png$/);
-
+        
         const response = await axios.get(result);
         expect(response.status).toBe(200); 
         
-    },10000);
+    },30000);
 });
