3364e13e2ab6d23cb51aecdcf01e60728a7600c9
pj <Pinjiang1220@gmail.com>
2024-05-14 18:39:45 +0800

    Replace userID with jwt token in post recipe API

--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const recipeController = require('../controller/recipeController');
 const router = express.Router();
+const auth = require("../utils/auth");
 
 router.post('/',
     recipeController.postRecipeValidator,
+    auth.verifyToken,
     recipeController.postRecipe
 );
 
