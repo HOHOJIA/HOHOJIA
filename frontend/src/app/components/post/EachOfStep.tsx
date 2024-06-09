e87a614ec90e44498ed2c6db8a03e5f933fe96dc
tsaichiehhuang <a0970605512@gmail.com>
2024-06-10 02:01:13 +0800

    fix(post): separate EachOfStep from Step

---
+++ frontend/src/app/components/post/EachOfStep.tsx
@@ -0,0 +1,56 @@
+import { Textarea } from '@nextui-org/react'
+import IconButton from './IconButton'
+import { FaPlus } from 'react-icons/fa6'
+import { IoTrash, IoReorderThreeOutline } from 'react-icons/io5'
+
+import DropZoneImg from './DropZoneImg'
+
+export function EachOfStep({
+    order,
+    id,
+    onClickAdd,
+    onClickDel,
+    description,
+    onChangeDescription,
+    getImgUrl,
+}: {
+    order: number
+    id: string
+    onClickAdd: () => void
+    onClickDel: () => void
+    description: string
+    onChangeDescription: (event: React.ChangeEvent<HTMLInputElement>, id: string) => void
+    getImgUrl: (file: File) => void
+}) {
+    return (
+        <div className="flex flex-wrap mt-5 mb-12 h-28">
+            <div className="flex items-end justify-between w-full mb-1 sm:items-center sm:justify-end">
+                <div className="flex justify-center w-1/6 text-2xl font-medium bg-yellow-300 rounded-lg sm:hidden">
+                    {order}
+                </div>
+                <div>
+                    <IconButton icon={FaPlus} size="1.2rem" onClick={onClickAdd} />
+                    <IconButton icon={IoTrash} size="1.2rem" onClick={onClickDel} />
+                    <IconButton icon={IoReorderThreeOutline} size="1.2rem" />
+                </div>
+            </div>
+
+            <div className="flex w-full gap-5 mt-1 flex-nowrap">
+                <div className="items-center justify-center hidden w-1/12 text-2xl font-medium bg-yellow-300 rounded-lg sm:flex h-28">
+                    {order}
+                </div>
+
+                <DropZoneImg smallSize={true} getImgUrl={getImgUrl} />
+
+                <Textarea
+                    className="w-8/12"
+                    minRows={3}
+                    label="輸入食譜描述"
+                    variant="bordered"
+                    value={description}
+                    onChange={(event) => onChangeDescription(event, id)}
+                />
+            </div>
+        </div>
+    )
+}
--- frontend/src/app/components/post/Steps.tsx
+++ frontend/src/app/components/post/Steps.tsx
@@ -1,10 +1,6 @@
-import { Textarea } from '@nextui-org/react'
-import IconButton from './IconButton'
-import { FaPlus } from 'react-icons/fa6'
-import { IoTrash, IoReorderThreeOutline } from 'react-icons/io5'
 import { useEffect, useRef } from 'react'
 import { v4 as uuidv4 } from 'uuid' // 引入 uuid lib
-import DropZoneImg from './DropZoneImg'
+import { EachOfStep } from './EachOfStep'
 
 export default function Steps({
     steps,
@@ -53,8 +49,6 @@ export default function Steps({
         setSteps(newStep)
     }
 
-    // console.log(steps);
-
     return (
         <div className="w-full">
             <p className="my-4 text-lg font-bold">步驟</p>
@@ -70,60 +64,6 @@ export default function Steps({
                     getImgUrl={getImgUrl}
                 />
             ))}
-        </div>
-    )
-}
-
-function EachOfStep({
-    order,
-    id,
-    onClickAdd,
-    onClickDel,
-    description,
-    onChangeDescription,
-    getImgUrl,
-}: {
-    order: number
-    id: string
-    onClickAdd: () => void
-    onClickDel: () => void
-    description: string
-    onChangeDescription: (event: React.ChangeEvent<HTMLInputElement>, id: string) => void
-    getImgUrl: (file: File) => void
-}) {
-    return (
-        <div className="flex flex-wrap mt-5 mb-12 h-28">
-            {/* icon */}
-            <div className="flex items-end justify-between w-full mb-1 sm:items-center sm:justify-end">
-                <div className="flex justify-center w-1/6 text-2xl font-medium bg-yellow-300 rounded-lg sm:hidden">
-                    {order}
-                </div>
-                <div>
-                    <IconButton icon={FaPlus} size="1.2rem" onClick={onClickAdd} />
-                    <IconButton icon={IoTrash} size="1.2rem" onClick={onClickDel} />
-                    <IconButton icon={IoReorderThreeOutline} size="1.2rem" />
-                </div>
-            </div>
-
-            <div className="flex w-full gap-5 mt-1 flex-nowrap">
-                {/* order */}
-                <div className="items-center justify-center hidden w-1/12 text-2xl font-medium bg-yellow-300 rounded-lg sm:flex h-28">
-                    {order}
-                </div>
-
-                {/* dropzone */}
-                <DropZoneImg smallSize={true} getImgUrl={getImgUrl} />
-
-                {/* textarea */}
-                <Textarea
-                    className="w-8/12"
-                    minRows={3}
-                    label="輸入食譜描述"
-                    variant="bordered"
-                    value={description}
-                    onChange={(event) => onChangeDescription(event, id)}
-                />
-            </div>
         </div>
     )
 }
