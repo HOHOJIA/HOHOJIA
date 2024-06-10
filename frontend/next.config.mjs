bad05a49b9c6d628007107e46cf2c42fca36944f
Yi-Jhen Jie <emily6331@gmail.com>
2024-06-10 21:32:26 +0800

    Feature: Add allowed image domains to Next.js config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
         missingSuspenseWithCSRBailout: false,
     },
     output: 'standalone',
+    images: {
+        domains: ['hohojia.s3.ap-southeast-2.amazonaws.com', 'images.dog.ceo'],
+    },
 }
 
 export default nextConfig
